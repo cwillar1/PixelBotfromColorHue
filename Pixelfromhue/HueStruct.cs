266ffb7c0879f005ec97eb09629059e6cdb37d5d
Colin Willard <Gobblgut123@gmail.com>
2023-05-07 13:26:43 -0400

    Structs moved to Huestruct.cs

---
+++ Pixelfromhue/HueStruct.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixelfromhue
+{
+    public class HueStruct
+    {
+        public struct CIEXYZ
+        {
+            /// <summary>
+            /// Gets an empty CIEXYZ structure.
+            /// </summary>
+            public static readonly CIEXYZ Empty = new CIEXYZ();
+            /// <summary>
+            /// Gets the CIE D65 (white) structure.
+            /// </summary>
+            public static readonly CIEXYZ D65 = new CIEXYZ(0.9505, 1.0, 1.0890);
+
+
+            public double x;
+            public double y;
+            public double z;
+
+            public static bool operator ==(CIEXYZ item1, CIEXYZ item2)
+            {
+                return (
+                    item1.X == item2.X
+                    && item1.Y == item2.Y
+                    && item1.Z == item2.Z
+                    );
+            }
+
+            public static bool operator !=(CIEXYZ item1, CIEXYZ item2)
+            {
+                return (
+                    item1.X != item2.X
+                    || item1.Y != item2.Y
+                    || item1.Z != item2.Z
+                    );
+            }
+
+            /// <summary>
+            /// Gets or sets X component.
+            /// </summary>
+            public double X
+            {
+                get
+                {
+                    return this.x;
+                }
+                set
+                {
+                    this.x = (value > 0.9505) ? 0.9505 : ((value < 0) ? 0 : value);
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets Y component.
+            /// </summary>
+            public double Y
+            {
+                get
+                {
+                    return this.y;
+                }
+                set
+                {
+                    this.y = (value > 1.0) ? 1.0 : ((value < 0) ? 0 : value);
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets Z component.
+            /// </summary>
+            public double Z
+            {
+                get
+                {
+                    return this.z;
+                }
+                set
+                {
+                    this.z = (value > 1.089) ? 1.089 : ((value < 0) ? 0 : value);
+                }
+            }
+
+            public CIEXYZ(double x, double y, double z)
+            {
+                this.x = (x > 0.9505) ? 0.9505 : ((x < 0) ? 0 : x);
+                this.y = (y > 1.0) ? 1.0 : ((y < 0) ? 0 : y);
+                this.z = (z > 1.089) ? 1.089 : ((z < 0) ? 0 : z);
+            }
+
+            public override bool Equals(Object obj)
+            {
+                if (obj == null || GetType() != obj.GetType()) return false;
+
+                return (this == (CIEXYZ)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+            }
+
+        }
+
+
+        public struct CIELab
+        {
+            /// <summary>
+            /// Gets an empty CIELab structure.
+            /// </summary>
+            public static readonly CIELab Empty = new CIELab();
+
+            public double l;
+            public double a;
+            public double b;
+
+
+            public static bool operator ==(CIELab item1, CIELab item2)
+            {
+                return (
+                    item1.L == item2.L
+                    && item1.A == item2.A
+                    && item1.B == item2.B
+                    );
+            }
+
+            public static bool operator !=(CIELab item1, CIELab item2)
+            {
+                return (
+                    item1.L != item2.L
+                    || item1.A != item2.A
+                    || item1.B != item2.B
+                    );
+            }
+
+
+            /// <summary>
+            /// Gets or sets L component.
+            /// </summary>
+            public double L
+            {
+                get
+                {
+                    return this.l;
+                }
+                set
+                {
+                    this.l = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets a component.
+            /// </summary>
+            public double A
+            {
+                get
+                {
+                    return this.a;
+                }
+                set
+                {
+                    this.a = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets a component.
+            /// </summary>
+            public double B
+            {
+                get
+                {
+                    return this.b;
+                }
+                set
+                {
+                    this.b = value;
+                }
+            }
+
+            public CIELab(double l, double a, double b)
+            {
+                this.l = l;
+                this.a = a;
+                this.b = b;
+            }
+
+            public override bool Equals(Object obj)
+            {
+                if (obj == null || GetType() != obj.GetType()) return false;
+
+                return (this == (CIELab)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return L.GetHashCode() ^ a.GetHashCode() ^ b.GetHashCode();
+            }
+
+        }
+
+    }
+}
--- Pixelfromhue/Program.cs
+++ Pixelfromhue/Program.cs
@@ -10,6 +10,7 @@ using System.Drawing;
 using System.Diagnostics;
 using WindowsInput;
 using WindowsInput.Native;
+using Pixelfromhue;
 class Program
 {
 
@@ -23,204 +24,7 @@ class Program
     public const byte KEYEVENTF_KEYUP = 0x02;
     public const byte VK_LMENU = 0xA4;
 
-    public struct CIEXYZ
-    {
-        /// <summary>
-        /// Gets an empty CIEXYZ structure.
-        /// </summary>
-        public static readonly CIEXYZ Empty = new CIEXYZ();
-        /// <summary>
-        /// Gets the CIE D65 (white) structure.
-        /// </summary>
-        public static readonly CIEXYZ D65 = new CIEXYZ(0.9505, 1.0, 1.0890);
-
-
-        public double x;
-        public double y;
-        public double z;
-
-        public static bool operator ==(CIEXYZ item1, CIEXYZ item2)
-        {
-            return (
-                item1.X == item2.X
-                && item1.Y == item2.Y
-                && item1.Z == item2.Z
-                );
-        }
-
-        public static bool operator !=(CIEXYZ item1, CIEXYZ item2)
-        {
-            return (
-                item1.X != item2.X
-                || item1.Y != item2.Y
-                || item1.Z != item2.Z
-                );
-        }
-
-        /// <summary>
-        /// Gets or sets X component.
-        /// </summary>
-        public double X
-        {
-            get
-            {
-                return this.x;
-            }
-            set
-            {
-                this.x = (value > 0.9505) ? 0.9505 : ((value < 0) ? 0 : value);
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets Y component.
-        /// </summary>
-        public double Y
-        {
-            get
-            {
-                return this.y;
-            }
-            set
-            {
-                this.y = (value > 1.0) ? 1.0 : ((value < 0) ? 0 : value);
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets Z component.
-        /// </summary>
-        public double Z
-        {
-            get
-            {
-                return this.z;
-            }
-            set
-            {
-                this.z = (value > 1.089) ? 1.089 : ((value < 0) ? 0 : value);
-            }
-        }
-
-        public CIEXYZ(double x, double y, double z)
-        {
-            this.x = (x > 0.9505) ? 0.9505 : ((x < 0) ? 0 : x);
-            this.y = (y > 1.0) ? 1.0 : ((y < 0) ? 0 : y);
-            this.z = (z > 1.089) ? 1.089 : ((z < 0) ? 0 : z);
-        }
-
-        public override bool Equals(Object obj)
-        {
-            if (obj == null || GetType() != obj.GetType()) return false;
-
-            return (this == (CIEXYZ)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
-        }
-
-    }
-
-
-    public struct CIELab
-    {
-        /// <summary>
-        /// Gets an empty CIELab structure.
-        /// </summary>
-        public static readonly CIELab Empty = new CIELab();
-
-        public double l;
-        public double a;
-        public double b;
-
-
-        public static bool operator ==(CIELab item1, CIELab item2)
-        {
-            return (
-                item1.L == item2.L
-                && item1.A == item2.A
-                && item1.B == item2.B
-                );
-        }
-
-        public static bool operator !=(CIELab item1, CIELab item2)
-        {
-            return (
-                item1.L != item2.L
-                || item1.A != item2.A
-                || item1.B != item2.B
-                );
-        }
-
-
-        /// <summary>
-        /// Gets or sets L component.
-        /// </summary>
-        public double L
-        {
-            get
-            {
-                return this.l;
-            }
-            set
-            {
-                this.l = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets a component.
-        /// </summary>
-        public double A
-        {
-            get
-            {
-                return this.a;
-            }
-            set
-            {
-                this.a = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets a component.
-        /// </summary>
-        public double B
-        {
-            get
-            {
-                return this.b;
-            }
-            set
-            {
-                this.b = value;
-            }
-        }
-
-        public CIELab(double l, double a, double b)
-        {
-            this.l = l;
-            this.a = a;
-            this.b = b;
-        }
-
-        public override bool Equals(Object obj)
-        {
-            if (obj == null || GetType() != obj.GetType()) return false;
-
-            return (this == (CIELab)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return L.GetHashCode() ^ a.GetHashCode() ^ b.GetHashCode();
-        }
-
-    }
-
+    
     static void Main(string[] args)
     {
 
@@ -243,7 +47,7 @@ class Program
         SendKeys.SendWait("{z}");
         keybd_event(VK_LMENU, 0, KEYEVENTF_KEYUP, 0);
     }
-    public static CIEXYZ RGBtoXYZ(int red, int green, int blue)
+    public static HueStruct.CIEXYZ RGBtoXYZ(int red, int green, int blue)
     {
         // normalize red, green, blue values
         double rLinear = (double)red / 255.0;
@@ -259,7 +63,7 @@ class Program
             1 + 0.055), 2.2) : (bLinear / 12.92);
 
         // converts
-        return new CIEXYZ(
+        return new HueStruct.CIEXYZ(
             (r * 0.4124 + g * 0.3576 + b * 0.1805),
             (r * 0.2126 + g * 0.7152 + b * 0.0722),
             (r * 0.0193 + g * 0.1192 + b * 0.9505)
@@ -277,22 +81,22 @@ class Program
     /// <summary>
     /// Converts CIEXYZ to CIELab.
     /// </summary>
-    public static CIELab XYZtoLab(CIEXYZ xyzL)
+    public static HueStruct.CIELab XYZtoLab(HueStruct.CIEXYZ xyzL)
     {
         double y = xyzL.y;
         double x = xyzL.x;
         double z = xyzL.z;
 
-        CIELab lab = CIELab.Empty;
+        HueStruct.CIELab lab = HueStruct.CIELab.Empty;
 
-        lab.L = 116.0 * Fxyz(y / CIEXYZ.D65.Y) - 16;
-        lab.A = 500.0 * (Fxyz(x / CIEXYZ.D65.X) - Fxyz(y / CIEXYZ.D65.Y));
-        lab.B = 200.0 * (Fxyz(y / CIEXYZ.D65.Y) - Fxyz(z / CIEXYZ.D65.Z));
+        lab.L = 116.0 * Fxyz(y / HueStruct.CIEXYZ.D65.Y) - 16;
+        lab.A = 500.0 * (Fxyz(x / HueStruct.CIEXYZ.D65.X) - Fxyz(y / HueStruct.CIEXYZ.D65.Y));
+        lab.B = 200.0 * (Fxyz(y / HueStruct.CIEXYZ.D65.Y) - Fxyz(z / HueStruct.CIEXYZ.D65.Z));
 
         return lab;
     }
 
-    public static double deltaECalc(CIELab lab1, CIELab lab2)
+    public static double deltaECalc(HueStruct.CIELab lab1, HueStruct.CIELab lab2)
     {
         //using CEI76 
         double totalL = lab2.L - lab1.L;
@@ -324,7 +128,7 @@ class Program
 
     }
 
-    public static void ParseScreen(Bitmap memoryImage, CIELab labL)
+    public static void ParseScreen(Bitmap memoryImage, HueStruct.CIELab labL)
     {
 
         for (int y = 0; y < 540; y+=10)
@@ -335,8 +139,8 @@ class Program
                 Color currentPixelColor = memoryImage.GetPixel(x, y);
 
                 //Color currentPixelColor = Color.FromArgb(214, 118, 72);
-                CIEXYZ xyzK = RGBtoXYZ(currentPixelColor.R, currentPixelColor.G, currentPixelColor.B);
-                CIELab labK = XYZtoLab(xyzK);
+                HueStruct.CIEXYZ xyzK = RGBtoXYZ(currentPixelColor.R, currentPixelColor.G, currentPixelColor.B);
+                HueStruct.CIELab labK = XYZtoLab(xyzK);
 
                 //calculate delta E from both labs
                 double deltaE = deltaECalc(labL, labK);
@@ -386,8 +190,8 @@ class Program
 
         //MAGENTA COLOR 255,0,255
         desiredColor = Color.FromArgb(255, 0, 255);
-        CIEXYZ xyzL = RGBtoXYZ(255, 0, 255);
-        CIELab labL = XYZtoLab(xyzL);
+        HueStruct.CIEXYZ xyzL = RGBtoXYZ(255, 0, 255);
+        HueStruct.CIELab labL = XYZtoLab(xyzL);
 
         ParseScreen(memoryImage, labL);
 
